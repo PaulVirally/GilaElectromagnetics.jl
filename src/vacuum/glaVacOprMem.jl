4b9430a64cdb391678c8a674d8a92613248676b1
Paul Virally <pvirally@gmail.com>
2025-05-07 12:42:43 -0400

    Convert CuArray to Array before serialization

--- src/vacuum/glaVacOprMem.jl
+++ src/vacuum/glaVacOprMem.jl
@@ -302,11 +302,13 @@ end
 # Add serialization support for GlaVacOprMem
 function Serialization.serialize(io::IO, mem::GlaVacOprMem)
     wasGpu = false
+    egoFur = mem.egoFur
     if mem.cmpInf isa GPUKerOpt
         wasGpu = true
         useCpu!(mem) # Convert to CPU for serialization
+        egoFur = collect(map(Array, mem.egoFur))
     end
-    serialize(io, mem.egoFur)
+    serialize(io, egoFur)
     serialize(io, mem.cmpInf)
     serialize(io, mem.trgVol)
     serialize(io, mem.srcVol)
